f006227b3967aaa30d8c3118acc8cc7a0dfe6756
Sayan Chakrabortty <sayanchakrabortty3@gmail.com>
2024-11-09 00:07:40 +0530

    initial GeoMap Application

---
+++ frontend/src/main.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+import { AuthContextprovider } from './context/AuthContext'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <AuthContextprovider>
+        <App />
+      </AuthContextprovider>
+    </BrowserRouter>
+  </React.StrictMode>,
+)
